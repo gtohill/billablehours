2d831e84ad373a6ee3361966dfe8104a0848c68d
gtohill@uwo.ca <Gt153328@>
2019-12-22 10:55:57 -0500

    update 2

--- resources/views/accounts/client.blade.php
+++ resources/views/accounts/client.blade.php
@@ -7,7 +7,6 @@
 <div class="container">
    
 </div>
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">    
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
